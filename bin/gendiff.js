e1a87b0a98f598ccd4c0c2f3e652b0f758c39679
Anna <anna.chubiy@gmail.com>
2020-06-20 17:13:22 +0300

    Add linter and GitHub Actions

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+
+const { program } = commander;
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import commander from 'commander';
+
+const { program } = commander;
+program.version('0.0.1');
+
+export default program;
