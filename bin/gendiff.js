a577a7440911de8c7251a0e55b3a65b3480db934
Anna <anna.chubiy@gmail.com>
2020-07-21 09:30:00 +0300

    Add --format option & arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-import commander from 'commander';
+import program from '../src/index.js';
 
-const { program } = commander;
-
-program
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+program();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import commander from 'commander';
 
 const { program } = commander;
-program.version('0.0.1');
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .parse(process.argv);
 
 export default program;
